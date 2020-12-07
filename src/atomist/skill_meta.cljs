ffbd034bbca4c70763eada852b0842310768eef3
Jim Clark <jim@atomist.com>
2020-12-07 15:34:23 -0800

    Bump clojars-integration to 0.1.4 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "clojars-integration")
 (def path ".")
-(def version "0.1.3")
-(def tag "0.1.3")
-(def generated-at "2020-12-07T23:10:22Z")
+(def version "0.1.4")
+(def tag "0.1.4")
+(def generated-at "2020-12-07T23:34:23Z")
